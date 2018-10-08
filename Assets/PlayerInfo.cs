427be686c9218bbb646990e4895cc379af9576cb
allie <alliequintano@gmail.com>
2018-10-08 15:04:03 -0500

    gives player experience points

---
+++ Assets/PlayerInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+    public float exp;
+
+	// Use this for initialization
+	void Start ()
+    {
+        exp = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        exp += 1 * Time.deltaTime;
+        Debug.Log(exp);
+	}
+}
